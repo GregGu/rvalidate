9168bf8d531d07971829195b932fb3f29e4e7f8b
greggu <gguranic@umass.edu>
2020-01-21 15:13:39 -0500

    subset added, not working yet

--- R/error_data.R
+++ R/error_data.R
@@ -8,20 +8,44 @@
 #' @return
 #' @export
 #'
-error_data <- function(data, parameter, estimate, totalerror_sd)
+error_data <- function(data, parameter, estimate, totalerror_sd, subset)
 {
-  resid <- data[[parameter]] - data[[estimate]]
-  e <- matrix(c(resid,
-                resid / sd(resid),
-                resid / (sqrt(1 / data[[totalerror_sd]]))), ncol = 3)
-  
-  edata <- data.frame(
-    "mean" = e %>% apply(2, mean),
-    "absolute_mean" = e %>% apply(2, abs) %>% apply(2, mean),
-    "median" = e %>% apply(2, median),
-    "absolute_median" = e %>% apply(2, abs) %>% apply(2, median)
-  )
-  rownames(edata) <- c("error", "standard error", "adjusted error")
-  edata
+  if (is.null(subset)) {
+    resid <- data[[parameter]] - data[[estimate]]
+    e <- matrix(c(resid,
+                  resid / sd(resid),
+                  resid / (1 / data[[totalerror_sd]])),
+                ncol = 3)
+    
+    edata <- data.frame(
+      "mean" = e %>% apply(2, mean),
+      "absolute_mean" = e %>% apply(2, abs) %>% apply(2, mean),
+      "median" = e %>% apply(2, median),
+      "absolute_median" = e %>% apply(2, abs) %>% apply(2, median)
+    )
+    rownames(edata) <- c("error", "standard error", "adjusted error")
+  } else {
+    sets <- unique(data[[subset]])
+    edata <- list() 
+    symsubset <- rlang::sym(subset)
+    for(i in 1:length(sets)) {
+      set <- sets[i]
+      tempdata <- data %>% dplyr::filter(!!symsubset == set)
+      resid <- tempdata[[parameter]] - tempdata[[estimate]]
+      e <- matrix(c(resid,
+                    resid / sd(resid),
+                    resid / (1 / tempdata[[totalerror_sd]])),
+                  ncol = 3)
+      
+      edatatemp <- data.frame(
+        "mean" = e %>% apply(2, mean),
+        "absolute_mean" = e %>% apply(2, abs) %>% apply(2, mean),
+        "median" = e %>% apply(2, median),
+        "absolute_median" = e %>% apply(2, abs) %>% apply(2, median)
+      )
+      rownames(edatatemp) <- c("error", "standard error", "adjusted error")
+      edata[[i]] <- edatatemp
+    }
+  }
   return(edata)
 }
--- vignettes/permutations.R
+++ vignettes/permutations.R
@@ -1,7 +1,6 @@
 temp <- c("1a", "1b", "2a", "2b", "2c")
 combinat::permn(temp)
-
-gtools::permutations(temp)
+# gtools::permutations(temp)
 
 
 #we have multiple bins of different size so total perm is
@@ -14,27 +13,27 @@ binl <- list(c(1,2), c(1,2), c(1,2,3))
 binl2 <- list(c(TRUE,TRUE), c(TRUE,TRUE), c(TRUE,TRUE,TRUE))
 
 
-1,1,1
-1,1,2
-1,1,3
-
-1,2,1
-1,2,2
-1,2,3
-
-2,1,1
-2,1,2
-2,1,3
-
-2,2,1
-2,2,2
-2,2,3
+# 1,1,1
+# 1,1,2
+# 1,1,3
+# 
+# 1,2,1
+# 1,2,2
+# 1,2,3
+# 
+# 2,1,1
+# 2,1,2
+# 2,1,3
+# 
+# 2,2,1
+# 2,2,2
+# 2,2,3
 
 
 # we have 2*3 permutations here in lexicographical order
 # is there a way to compute this list in R?
 # After spending some time on my attempt I found that this has been worked out 
-# using the factoraic of a number .... we will skip to the brute force method for now
+# using the factoradic of a number .... we will skip to the brute force method for now
 # 
 #   # attempt
 # 
@@ -72,6 +71,19 @@ binl2 <- list(c(TRUE,TRUE), c(TRUE,TRUE), c(TRUE,TRUE,TRUE))
 # - if unique = total number computed earlier stop
 # - else repeat whole process
 
+library(dplyr)
+
+  # we have some colum in our data with unique codes and 0s for non dupes
+  cexam <- c(4002001, 0, 4002001, 4502000, 4502000, 4502000, 0)
+  # convert to a number system where 0 is non dupe and anything greater is a dupe
+  cef <- cexam %>% as.factor() %>% as.numeric() - 1
+  # 1 will be the first index in the list and so on ...
+  binl <- list()
+  cef2 <- cef[cef > 0]
+  for (i in unique(cef2)) {
+    binl[[i]] <- 1:((cef == i) %>% sum())
+  }
+
 
 binl <- list(c(1,2), c(1,2), c(1,2,3))
 
@@ -96,5 +108,22 @@ while (length(permutations) < totalnumber) {
   i <- i + 1
 }
 
+# now that we have permuations how to sample from data?
+# the index in a single permutation corresponds to which bin
+# the number at the index corresponds to which dupe
+# all 0s are sampled
+
+# sinx is our abbreviation for sample index
+# sinxl is the list of these samples
+for(i in 1:length(bins)) { # each i here is going to be a set of samples
+  sinx
+  for(j in 1:length(bin[[i]])) {
+    dinx <- which(cef == i) # the positions of all dupes for this bin in the data
+    dinx2 <- bin[[i]][[j]] # which observation of this dupe we want to sample
+    dinx3 <- dinx[dinx2] # the index in the data of obs
+
+  }
+
+}
 
 
